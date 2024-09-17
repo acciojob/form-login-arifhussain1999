4093d9448ca24073f2edf334459857d8561bd218
Arif Hussain <arifh9821@gmail.com>
2024-09-17 15:43:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
+	event.preventDefault();
+	var firstName=document.getElementsByName("fname")[0].value;
+	var lastName=document.getElementsByName("lname")[0].value;
+
+	alert(firstName + " " + lastName);
 
 }
