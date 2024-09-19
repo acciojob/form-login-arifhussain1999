42c4a3494b42ada906da315b4b3fd769a038f81e
Arif Hussain <arifh9821@gmail.com>
2024-09-19 13:02:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function getFormvalue() {
     //Write your code here
-	
-	var firstName=document.getElementsByName("fname")[0].value;
-	var lastName=document.getElementsByName("lname")[0].value;
+
+	const form = document.getElementById("form1");
+
+	const formData = new FormData(form);
+
+	const firstName = formData.get('fname');
+	const lastName = formData.get('lname');
 
 	alert(firstName + " " + lastName);
+	// var firstName=document.getElementsByName("fname")[0].value;
+	// var lastName=document.getElementsByName("lname")[0].value;
+
+	// alert(firstName + " " + lastName);
 
 }
